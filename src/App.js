f917e9c74c5b986ea89fffbe9341e6f9c3bc04f6
yousofzyadna <82836927+yousofzyadna@users.noreply.github.com>
2021-04-19 21:54:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Products from './Products';
+
+
+
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+     <Header/>
+      <Products/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Products.js
@@ -0,0 +1,19 @@
+import Product from "./Product";
+import "./App.css";
+function Products() {
+  return (
+    <section className="products">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+}
+export default Products;
